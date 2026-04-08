3964d14995ddde747a8ab7a464681fa533e855a2
Gonzalo <gonzaloriosbarcelo@gmail.com>
2026-04-07 21:19:39 -0300

    Empezamos la app de streamlit.

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -2,10 +2,36 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 
-BASE_DIR = Path.cwd().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_PATH = BASE_DIR / "data" / "processed"
-FILE_PATH = DATA_PATH / "car-dataset-cleaned-2025.csv"
 
-df = pd.read_csv(FILE_PATH)
-
-df
+try:
+    file = DATA_PATH / "car-dataset-cleaned-2025.csv"
+    df = pd.read_csv(file)
+    # Streamlit App
+    st.set_page_config(page_title="Home",
+                       page_icon=":home:",
+                       layout="wide",
+                       initial_sidebar_state="auto")
+    # Title of the home page
+    st.title("Car Analytics Dashboard",
+             text_alignment="center")
+    # KPIs
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.metric(label="Average Price",
+                  value=round(df["price"].mean(), 2),
+                  border=True,
+                  format="dollar")
+    with col2:
+        st.metric(label="Average Horse Power",
+                  value=round(df["horse_power"].mean(), 2),
+                  border=True)
+    with col3:
+        st.metric(label="Average Top Speed",
+                  value=round(df["max_speed"].mean(), 2),
+                  border=True)
+    # st.metric(label="Electric Cars Percentage")
+        
+except FileNotFoundError:
+    print("El archivo o la ruta no existen.")
