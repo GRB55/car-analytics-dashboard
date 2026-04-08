0722514f3f16c2334afcfb5a700b855547143ef5
Gonzalo <gonzaloriosbarcelo@gmail.com>
2026-04-08 20:23:47 -0300

    Renombramos todos los archivos del proyecto.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 DATA_PATH = BASE_DIR / "data" / "processed" / "car-dataset-cleaned-2025.csv"
 
 try:
@@ -16,16 +16,16 @@ try:
     # Title of the home page
     st.title("Car Analytics Dashboard",
              text_alignment="center")
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(5)
+    with tab1:
         st.image(r"Car-analytics-dashboard\images\audi.png", width=300)
-    with col2:
+    with tab2:
         st.image(r"Car-analytics-dashboard\images\ferrari.png", width=220)
-    with col3:
+    with tab3:
         st.image(r"Car-analytics-dashboard\images\ford.png", width=300)
-    with col4:
+    with tab4:
         st.image(r"Car-analytics-dashboard\images\lamborghini.png", width=250)
-    with col5:
+    with tab5:
         st.image(r"Car-analytics-dashboard\images\mercedes.png", width=300)
     # KPIs
     col6, col7, col8, col9 = st.columns(4)
--- pages/1_Explorador.py
+++ pages/1_Explorador.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from pathlib import Path
+plt.style.use("dark_background")
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_PATH = BASE_DIR / "data" / "processed" / "car-dataset-cleaned-2025.csv"
@@ -17,9 +20,16 @@ try:
     st.title("Exploratory Data Analysis",
              text_alignment="center")
     
-    container = st.container()
+    container = st.container(gap=None)
     with container:
-        st.bar_chart(data=df, y="brand")
-    
+        plt.figure(figsize=(9, 5))
+        sns.countplot(data=df, y="brand", hue="brand", order=df['brand'].value_counts().index, palette="viridis")
+        plt.title("Marcas con mayor cantidad de apariciones")
+        plt.xlabel("")
+        plt.ylabel("")
+        plt.tight_layout()
+        st.pyplot(plt)
+        st.divider()
+        
 except FileNotFoundError:
     print("El archivo o la ruta no existen.")
