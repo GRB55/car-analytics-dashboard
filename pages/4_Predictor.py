f03a28b6448f020245979b56b844dfba54b855df
Gonzalo <gonzaloriosbarcelo@gmail.com>
2026-04-10 14:09:05 -0300

    Solucion de problema.

--- pages/4_Predictor.py
+++ pages/4_Predictor.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import mean_squared_error, root_mean_squared_error, r2_score
-from sklearn.preprocessing import StandardScaler,
+from sklearn.preprocessing import StandardScaler
 from utils import load_data
 
 # frecuency encoder -> frecuencia de aparicion de la categoria
