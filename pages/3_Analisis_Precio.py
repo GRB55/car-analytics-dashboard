e820809990a4eefcddd4ddaa0349db4840e17198
Gonzalo <gonzaloriosbarcelo@gmail.com>
2026-04-10 14:50:28 -0300

    Grafico precios marcas.

--- pages/3_Analisis_Precio.py
+++ pages/3_Analisis_Precio.py
@@ -13,5 +13,17 @@ try:
     # Title of the home page
     st.title("Análisis de Precios",
              text_alignment="center")
+    tab1, tab2, tab3 = st.tabs(["Marca", "Combustible", "Correlaciones"])
+    with tab1:
+        marcas_ordenadas = df.groupby("brand")["price"].mean().sort_values(ascending=False).index
+        fig = px.histogram(data_frame=df, 
+                           x="brand", y="price", orientation="v", 
+                           color_discrete_sequence=['indianred'], 
+                           histfunc="avg", category_orders=dict(brand = marcas_ordenadas))
+        fig.update_layout(
+            xaxis_title_text = "Marca",
+            yaxis_title_text = "Precio Promedio"
+        )
+        st.plotly_chart(fig)
 except FileNotFoundError:
     print("El archivo o la ruta no existen.")
