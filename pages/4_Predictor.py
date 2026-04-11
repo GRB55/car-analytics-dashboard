8fe62cfe15e6f4a0529b10c8ee6556e5747904ad
Gonzalo <gonzaloriosbarcelo@gmail.com>
2026-04-10 21:12:06 -0300

    Comenzamos pagina final.

--- pages/4_Predictor.py
+++ pages/4_Predictor.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split, GridSearchCV
@@ -30,4 +31,19 @@ tgt_encoder = TargetEncoder()
 try:
     # Guardar el dataset en un marco de datos
     df = load_data()
-    # Caracteristicas y objetivo
+    # Preprocesamiento de datos
+    for col in df.select_dtypes(include=object):
+        if (df[col].nunique() < 32):       
+            encoded_col = oh_encoder.fit_transform(df[[col]])
+            encoded_df = pd.DataFrame(columns=oh_encoder.get_feature_names_out(),
+                                      data=encoded_col,
+                                      index=df.index)
+            df = pd.concat([df, encoded_df], axis=1)
+            df = df.drop(col, axis=1)
+    # Características y objetivo
+    X = df.drop("price", axis=1)
+    y = df["price"]
+    # Conjuntos de entrenamiento y prueba
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+except FileNotFoundError:
+    print("El archivo o la ruta no existen.")
